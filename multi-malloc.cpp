4aa51789947a641e3b7d28654c636643c313b0da
ysh8361 <sea5727@naver.com>
2021-01-24 20:49:45 +0900

    commit

---
+++ multi-malloc.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <thread>
+#include <vector>
+#include <memory.h>
+
+
+int main(int argc, char* argv[]) {
+
+    if(argc < 4){
+        return 0;
+    }
+
+    int COUNT = ::atoi(argv[1]);
+    int SIZE = ::atoi(argv[2]);
+    int THREAD = ::atoi(argv[3]);
+
+    printf("COUNT:%d, SIZE:%d, THREAD:%d\n", COUNT, SIZE, THREAD);
+    
+    std::vector<std::thread> workers;
+    std::vector<std::thread> workers2;
+    auto start1 = std::chrono::steady_clock::now();
+
+    char ***arr;
+    arr = (char ***)malloc(sizeof(char **) * THREAD);
+
+    for(int i = 0 ; i < THREAD ; ++i){
+        workers.emplace_back([=]{
+            arr[i] = (char **) malloc(sizeof(char *) * COUNT);
+            for(int j = 0 ; j < COUNT ; ++j){
+                arr[i][j] = (char *)malloc(sizeof(char) * SIZE);
+            }            
+        });
+    }
+
+    for(auto & worker : workers){
+        worker.join();
+    }
+
+    auto end1 = std::chrono::steady_clock::now();
+    auto diff1 = end1 - start1;
+    std::cout << std::chrono::duration <double, std::milli> (diff1).count() << " ms" << std::endl;
+
+    
+    auto start2 = std::chrono::steady_clock::now();
+
+    for(int i = 0 ; i < THREAD ; ++i){
+        workers2.emplace_back([=]{
+            for(int j = 0 ; j < COUNT ; ++j){
+                free(arr[i][j]);
+            }            
+            free(arr[i]);
+        });
+    }
+    free(arr);
+
+    for(auto & worker : workers2){
+        worker.join();
+    }
+
+    auto end2 = std::chrono::steady_clock::now();
+    auto diff2 = end2 - start2;
+    std::cout << std::chrono::duration <double, std::milli> (diff2).count() << " ms" << std::endl;
+
+
+    return 0;
+}
+
---
+++ single-malloc.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <thread>
+#include <memory.h>
+
+int main(int argc, char* argv[]) {
+
+    if(argc < 3){
+        return 0;
+    }
+
+    int COUNT = ::atoi(argv[1]);
+    int SIZE = ::atoi(argv[2]);
+
+    printf("COUNT:%d, SIZE:%d\n", COUNT, SIZE);
+
+    auto start = std::chrono::steady_clock::now();
+
+    // int COUNT = 1000000;
+    // int SIZE = 4069;
+    char * arr[COUNT];
+    for(int i = 0 ; i < COUNT ; ++i){
+        arr[i] = (char * )malloc(SIZE);
+    }
+    auto end1 = std::chrono::steady_clock::now();
+
+    for(int i = 0 ; i < COUNT ; ++i){
+        free(arr[i]);
+    }
+    auto end2 = std::chrono::steady_clock::now();
+
+    
+    auto diff1 = end1 - start;
+    auto diff2 = end2 - end1;
+    std::cout << std::chrono::duration <double, std::milli> (diff1).count() << " ms" << std::endl;
+    std::cout << std::chrono::duration <double, std::milli> (diff2).count() << " ms" << std::endl;
+    return 0;
+}
